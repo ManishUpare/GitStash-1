7bb42af12834dc8133e97d41530c4a1994865a8f
ManishUpare <mnupare12@gmail.com>
2023-02-06 16:44:42 +0530

    Change

---
+++ src/main/java/com/BikkadIT/AdditionLogicRequired/GitStash1Application.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.AdditionLogicRequired;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitStash1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitStash1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/BikkadIT/AdditionLogicRequired/GitStash1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.AdditionLogicRequired;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitStash1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
